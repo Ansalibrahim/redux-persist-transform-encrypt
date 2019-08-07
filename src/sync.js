d0cb528f93644759ba7885410dac45b901096cc0
Ansalibrahim <ansalibrahim@gmail.com>
2019-08-07 13:10:54 +0530

    Update sync.js

--- src/sync.js
+++ src/sync.js
@@ -6,13 +6,18 @@ import { makeEncryptor, makeDecryptor } from './helpers'
 const makeSyncEncryptor = secretKey =>
   makeEncryptor(state => AES.encrypt(state, secretKey).toString())
 
-const makeSyncDecryptor = (secretKey, onError) =>
+const makeSyncDecryptor = (secretKey, oldSecretKey, onError) =>
   makeDecryptor(state => {
     try {
       const bytes = AES.decrypt(state, secretKey)
       const decryptedString = bytes.toString(CryptoJSCore.enc.Utf8)
       return JSON.parse(decryptedString)
     } catch (err) {
+      if (oldSecretKey) {
+         const bytes = AES.decrypt(state, oldSecretKey)
+         const decryptedString = bytes.toString(CryptoJSCore.enc.Utf8)
+         return JSON.parse(decryptedString)
+      }
       throw new Error(
         'Could not decrypt state. Please verify that you are using the correct secret key.'
       )
@@ -21,6 +26,6 @@ const makeSyncDecryptor = (secretKey, onError) =>
 
 export default config => {
   const inbound = makeSyncEncryptor(config.secretKey)
-  const outbound = makeSyncDecryptor(config.secretKey, config.onError)
+  const outbound = makeSyncDecryptor(config.secretKey, config.oldSecretKey, config.onError)
   return createTransform(inbound, outbound, config)
 }
